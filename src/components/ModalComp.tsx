6fb5fdb3fe0911024ca0ad61b5b4890744931bda
Ganapathy Karthikeyan <rganapathykarthikeyan8@gmail.com>
2024-02-08 01:09:01 +0530

    Modal BluePrint Created

---
+++ src/components/ModalComp.tsx
@@ -0,0 +1,33 @@
+import { useTheme } from '@react-navigation/native';
+import React from 'react'
+import { Modal, Pressable, ScrollView, View, Text } from 'react-native'
+import TaskView from './TaskView';
+import { task } from '../store/tasksContext';
+
+const ModalComp = (props:any) => {
+    const theme = useTheme();
+    const background = theme.dark ? "#040d129f" : "#fffbf5a2";
+    const modalColor = theme.dark ? "#183d3dad" : "#7843dbad";
+    const List:task[] = props.taskList;
+  return (
+    <Modal
+        animationType="slide"
+        transparent={true}
+        visible={props.modalVisible}
+        onRequestClose={() => {
+            props.setModalVisible(false);
+        }}>
+        {/* Pressable is used to close the modal when user presses outside of the modal. Event is used to check if its clildren is pressed */}
+        <Pressable onPress={(event) => event.target == event.currentTarget && props.setModalVisible(false)} style={{ backgroundColor: background, flex: 1 }}>
+          <View style={{ backgroundColor: modalColor, opacity: 0.8, height: "90%", width: "100%", marginTop: "20%", borderRadius: 40 }}>
+            <Text>{props.compOrFuct}</Text>
+            <ScrollView>
+              {List.map(task => <TaskView task={task} key={task.taskId} />)}
+            </ScrollView>
+          </View>
+        </Pressable>
+      </Modal>
+  )
+}
+
+export default ModalComp
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import { useFocusEffect, useTheme } from '@react-navigation/native';
 import React, { useContext, useEffect, useState } from 'react'
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View, Modal, Pressable } from 'react-native';
 import { TaskContext, task } from '../store/tasksContext';
 import TaskView from '../components/TaskView';
 
@@ -8,11 +8,16 @@ import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
 import DoubleTick from '../assets/doubleTick.svg'
 import Future from '../assets/future.svg'
+import ModalComp from '../components/ModalComp';
 
 const MainPage = () => {
   const theme = useTheme();
   const context = useContext(TaskContext);
   const [todayList, setTodayList] = useState<task[]>([])
+  const [modalVisible, setModalVisible] = useState(false);
+  const background = theme.dark ? "#040d129f" : "#fffbf5a2";
+  const modalColor = theme.dark ? "#183d3dad" : "#7843dbad";
+  const [compOrFuct, setCompOrFuct] = useState<string>("Completed")
 
   useFocusEffect(
     React.useCallback(() => {
@@ -44,7 +49,7 @@ const MainPage = () => {
         <Text>+31%</Text>
       </View>
       <View style={styles.footer}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => { setCompOrFuct("Completed"); setModalVisible(true); }}>
           <DoubleTick height={20} width={20} color={theme.colors.card} />
         </TouchableOpacity>
         <View style={styles.circle}>
@@ -58,11 +63,28 @@ const MainPage = () => {
             onAnimationComplete={() => console.log('onAnimationComplete')}
             backgroundColor={theme.colors.border} />
         </View>
-        <Text style={{color:theme.colors.card, ...styles.percent}}>73%</Text>
-        <TouchableOpacity>
-        <Future height={20} width={20} color={theme.colors.card} />
+        <Text style={{ color: theme.colors.card, ...styles.percent }}>73%</Text>
+        <TouchableOpacity onPress={() => { setCompOrFuct("Future"); setModalVisible(true); }}>
+          <Future height={20} width={20} color={theme.colors.card} />
         </TouchableOpacity>
       </View>
+      <ModalComp modalVisible={modalVisible} setModalVisible={setModalVisible} compOrFuct={compOrFuct} taskList={todayList}/>
+      {/* <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}>
+        <Pressable onPress={(event) => event.target == event.currentTarget && setModalVisible(false)} style={{ backgroundColor: background, flex: 1 }}>
+          <View style={{ backgroundColor: modalColor, opacity: 0.8, height: "90%", width: "100%", marginTop: "20%", borderRadius: 40 }}>
+            <Text>{compOrFuct}</Text>
+            <ScrollView>
+              {todayList.map(task => <TaskView task={task} key={task.taskId} />)}
+            </ScrollView>
+          </View>
+        </Pressable>
+      </Modal> */}
     </View>
   )
 }
